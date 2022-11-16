06161dace4462b99cbd4b84feb0d00affb41042d
agustin panizza <agustin_panizza@hotmail.com>
2022-11-15 21:18:44 -0300

    front

--- hackathon/Api/src/routes/private/coursesPrivate.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-
-const { protect } = require("../../middleware/protect.js");
-const { createCurso } = require("../../controllers/controllerCourses.js");
-
-router.post("/", protect, createCurso);
-
-module.exports = router;
--- hackathon/Api/src/routes/private/roomsPrivate.js
+++ hackathon/Api/src/routes/private/roomsPrivate.js
@@ -3,15 +3,15 @@ const router = Router();
 
 const { protect } = require("../../middleware/protect.js");
 const {
-  createRoom,
-  getRoomById,
-  editRoom,
-  deleteRoom,
+  createTeam,
+  getTeamById,
+  editTeam,
+  deleteTeam,
 } = require("../../controllers/controllerRooms");
 
-router.post("/", protect, createRoom);
-router.get("/:id", protect, getRoomById);
-router.put("/:id/edit", protect, editRoom);
-router.delete("/deleteRoom", protect, deleteRoom);
+router.post("/", protect, createTeam);
+router.get("/:id", protect, getTeamById);
+router.put("/:id/edit", protect, editTeam);
+router.delete("/deleteRoom", protect, deleteTeam);
 
 module.exports = router;
